d3f82a6b62ea6084ef377e671b309e9632f4c2fd
Emil-Ebrahimi <48281894+Emil-Ebrahimi@users.noreply.github.com>
2021-12-16 14:06:31 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+	<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name='robots' content='Contact, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1' />
+<meta property="og:title" content="Contact"/>
+<meta property="og:description" content="CUSTOM WRAPS		
+			OVER ONS		
+		Warry Wraps is een gespecialiseerd en servicegericht auto wrapping bedrijf.Custom Wraps is een gespecialiseerd en servicegericht"/>
+<meta name="twitter:description" content="CUSTOM WRAPS		
+			OVER ONS		
+		Warry Wraps is een gespecialiseerd en servicegericht auto wrapping bedrijf.Custom Wraps is een gespecialiseerd en servicegericht"/>
+<meta name="author" content="Emil"/>
+
+	<title>Auto wrap en bedrijfsreclame specialist | Auto laten wrappen in Amsterdam</title>
+	<meta name="description" content="Warry Wraps " />
+	<link rel="canonical" href="index.html" />
+	<meta property="og:url" content="Contact" />
+	<meta property="og:site_name" content="Warry Wraps" />
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel='stylesheet' href='css/main.css' type='text/css'/>
+	<link rel='stylesheet' href='css/sub.css' type='text/css'/>
+<link rel='stylesheet' id='google-fonts-1-css'  href='https://fonts.googleapis.com/css?family=Fira+Sans%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CFira+Sans+Condensed%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&amp;display=auto&amp;ver=5.8.1' type='text/css' media='all' />
+<!-- <script src="https://use.fontawesome.com/e9f21839d4.js"></script> -->
+<script src="https://kit.fontawesome.com/93d286b065.js" crossorigin="anonymous"></script>
+
+</head>
+
+
+<body>
+	
+<section class="front">
+	<div class="d-none d-lg-flex row justify-content-between topbar">
+		<div class="col-10">
+			<a href="tel:0612345678"><i class="fas fa-phone-alt">
+				 &nbsp 0612345678</i></a>
+			<a href="mailto:warrywarry@hotmail.com"><i class="fas fa-envelope">  
+				&nbsp warrywarry@hotmail.com</i></a>
+		</div>
+		<div class="socials col-2">
+			<a href=""><i class="fab fa-instagram"></i></a>
+			<a href=""><i class="fab fa-facebook-square"></i></a>
+			<a href=""><i class="fab fa-linkedin"></i></a>
+			<a href="https://wa.me/0612345678" target="_blank">
+				<i class="fab fa-whatsapp"></i></a>
+		</div>
+	</div>
+		
+		
+	<nav class="navbar navbar-expand-lg navbar-dark bg-light">
+		<a class="navbar-brand" href="index.html">Navbar</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		  <span class="navbar-toggler-icon"></span>
+		</button>
+	  
+		<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+		  <ul class="navbar-nav col-3">
+			<li class="nav-item active">
+			  <a class="nav-link" href="index.html">Home</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#">Portfolio</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#">Diensten<span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="contact.html">Contact</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link disabled" href="#">Prijzen</a>
+			</li>
+		  </ul>
+		</div>
+	  </nav>
+
+	  <div class="row align-items-center justify-content-center Banner">
+		<div class="col-12 banner-titel text-center">
+		  <h1>Contact</h1>
+		</div>
+	</div>
+</section>
+
+<div class="container mt-5">
+	<h1>Contact Me!</h1>
+	<form class="row g-3" action="php/main.php" method="POST">
+	  <!-- Honeypot -->
+	  <input type="text" name="_honey" style="display: none;">
+
+	  <!-- Disable Captcha -->
+	  <input type="hidden" name="_captcha" value="false">
+
+	  <input type="hidden" name="_next" value="https://emil-ebrahimi.github.io/warrywraps/success.html" >
+
+	  <div class="col-md-6">
+		<label for="firstName" class="form-label">Voornaam</label>
+		<input type="text" class="form-control" name="firstname" id="firstName" required>
+	  </div>
+	  <div class="col-md-6">
+		<label for="lastName" class="form-label">Achternaam</label>
+		<input type="text" class="form-control" name="lastname" id="lastName" required>
+	  </div>
+	  <div class="col-md-8">
+		<label for="emailInfo" class="form-label">E-mail</label>
+		<input type="email" class="form-control" name="email" id="emailInfo" required>
+	  </div>
+	  <div class="col-md-4">
+		<label for="phoneNumber" class="form-label">Telefoon Nummer</label>
+		<input type="text" class="form-control" name="phone" id="phoneNumber" placeholder="+1 (415) 867-5309">
+	  </div>
+	  <div class="col-md-12">
+		<label for="comments" class="form-label">Uw Bericht</label>
+		<textarea class="form-control" id="comments" name="bericht" rows="3" required></textarea>
+	  </div>
+	  <div class="col-md-12">
+		<input type="submit" name="submit" class="btn btn-primary" />
+	  </div>
+	</form>
+</div>
+
+	
+
+
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  
+	<!-- Section: Links  -->
+	<section class="">
+	  <div class="container text-center text-md-start pt-4 mt-5">
+		<!-- Grid row -->
+		<div class="row mt-3">
+		  <!-- Grid column -->
+		  <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+			<!-- Content -->
+			<h6 class="text-uppercase fw-bold mb-4">
+			  <i class="fas fa-gem me-3"></i> Warry Wraps
+			</h6>
+			<p>
+			  Hier kan een korte beschrijving komen over Warry Wraps. Lorem ipsum
+			  dolor sit amet, consectetur adipisicing elit.
+			</p>
+		  </div>
+		  <!-- Grid column -->
+  
+		  <!-- Grid column -->
+		  <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+			<!-- Links -->
+			<h6 class="text-uppercase fw-bold mb-4">
+			  Diensten
+			</h6>
+			<p>
+			  <a href="carwrapping.html" class="text-reset">Carwrapping</a>
+			</p>
+			<p>
+			  <a href="ramenblinderen.html" class="text-reset">Ramen blinderen</a>
+			</p>
+			<p>
+			  <a href="lichtensmoken.html" class="text-reset">Lichten smoken</a>
+			</p>
+			<p>
+			  <a href="devicewrapping.html" class="text-reset">Device wrapping</a>
+			</p>
+		  </div>
+		  <!-- Grid column -->
+  
+		  <!-- Grid column -->
+		  <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+			<!-- Links -->
+			<h6 class="text-uppercase fw-bold mb-4">
+			  Overige diensten
+			</h6>
+			<p>
+			  <a href="ontchromen.html" class="text-reset">Ontchromen</a>
+			</p>
+			<p>
+			  <a href="detailwrapping.html" class="text-reset">Detail wrapping</a>
+			</p>
+			<p>
+			  <a href="kantoordecoratie.html" class="text-reset">Kantoor decoratie</a>
+			</p>
+			<p>
+			  <a href="bedrijfsreclame.html" class="text-reset">Bedrijfsreclame</a>
+			</p>
+		  </div>
+		  <!-- Grid column -->
+  
+		  <!-- Grid column -->
+		  <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+			<!-- Links -->
+			<h6 class="text-uppercase fw-bold mb-4">
+			  Contact
+			</h6>
+			<p><i class="fas fa-home me-3"></i> Amsterdam, 0000 AB, NL</p>
+			<p>
+			  <i class="fas fa-envelope me-3"></i>
+			  info@warrywarry.nl
+			</p>
+			<p><i class="fas fa-phone me-3"></i> +31 6 123 456 679</p>
+			<p><i class="fas fa-print me-3"></i> +31 234 567 89</p>
+		  </div>
+		  <!-- Grid column -->
+		</div>
+		<!-- Grid row -->
+	  </div>
+	</section>
+	<!-- Section: Links  -->
+  
+	<!-- Copyright -->
+	<div class="text-center p-2 copyright" style="background-color: #231F20;">
+	  <p>© 2021 Copyright: ZuZu's</p>
+	</div>
+	<!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script> -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="js/parallax.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js" integrity="sha256-dHf/YjH1A4tewEsKUSmNnV05DDbfGN3g7NMq86xgGh8=" crossorigin="anonymous"></script>
+</body>
+</html>
--- php/main.php
+++ php/main.php
@@ -1,17 +1,25 @@
 <?php
-$to = "dropyounow@gmail.com";
-$txt = "Hello world!";
-$headers = "From: $email" . "\r\n" .
-"CC: somebodyelse@example.com";
-
 if(isset($_POST['submit'])){
+    $to = "dropyounow@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['firstname'];
+    $last_name = $_POST['lastname'];
+    $subject = "Form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['bericht'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
 
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['emailInfo'];
-    $bericht = $_POST['comments']
-    mail($to,$bericht,$firstname);
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message);
     echo "gelukt!";
-    
-     }
+}
+
 ?>
+
+<html>
+<body>
+<?php echo $_POST['firstname'];?>
+<h1>Poep</h1>
+</body>
+</html>
+
